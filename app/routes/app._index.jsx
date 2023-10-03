5379a67c55eec92048ac95324a323fa72ebcd40f
JoyMatubber1335 <joy.matubber@brainstation-23.com>
2023-10-03 15:03:47 +0600

    Get Singel product

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,13 +1,22 @@
 import { json } from "@remix-run/node";
-import { Page } from "@shopify/polaris";
+import { Page, Card } from "@shopify/polaris";
 
 import { authenticate } from "../shopify.server";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ request }) => {
-  const id = 8716496830778;
+  const id = "8606462607660";
   const { admin, session } = await authenticate.admin(request);
-  const shop = await admin.rest.resources.Product.all({ session: session });
+  // const shop = await admin.rest.resources.Product.all({ session: session });
+  // cosnt shop=await admin.rest.Product.find({
+  //   session: session,
+  //   id: 632910392,
+  // });
+
+  const shop = await admin.rest.resources.Product.find({
+    session: session,
+    id: id,
+  });
   return json(shop);
 };
 
@@ -15,5 +24,12 @@ export default function Index() {
   const loaderData = useLoaderData();
 
   console.log(loaderData);
-  return <Page></Page>;
+  return (
+    <Page>
+      {/* {loaderData.map((product) => {
+        return <Card>{product.title}</Card>;
+      })} */}
+      ok
+    </Page>
+  );
 }
